d65c5e1d0e7e2a1c4eadc5fb9d176bbd35e40c47
limsungjun <lsjung567@naver.com>
2022-05-11 14:27:57 +0900

    add pinecone code

--- pet_image_similarity_search/pinecone_service.py
+++ pet_image_similarity_search/pinecone_service.py
@@ -1,50 +1,36 @@
 import pinecone
 import os
 
+# api_key
 api_key = os.getenv("PINECONE_API_KEY") or "4e5fd852-89e4-44fb-9e33-d605b79aba2f"
 pinecone.init(api_key=api_key, environment='us-west1-gcp')
 
+# version compatability
 import pinecone.info
-
 version_info = pinecone.info.version()
 server_version = ".".join(version_info.server.split(".")[:2])
 client_version = ".".join(version_info.client.split(".")[:2])
 
 assert client_version == server_version, "Please upgrade pinecone-client."
 
-# Choosing an arbitrary name for my index
+# Choosing an arbitrary name for my index: 인덱스 이름 지정
 index_name = "simple-pytorch-dog-search"
 
-# Checking whether the index already exists.
+# Checking whether the index already exists.: 인덱스 존재여부확인
 if index_name not in pinecone.list_indexes():
-    pinecone.create_index(index_name, dimension=1000, metric="euclidean")
+    pinecone.create_index(index_name, dimension=1000, metric="euclidean") #없으면 만들기 -> 여기서 cosine similarity 사용 가능
 
-index = pinecone.Index(index_name=index_name)
+index = pinecone.Index(index_name=index_name) # 인덱스 연결하기
 
-
-import itertools
-
-def chunks(iterable, batch_size=100):
-    it = iter(iterable)
-    chunk = tuple(itertools.islice(it, batch_size))
-    while chunk:
-        yield chunk
-        chunk = tuple(itertools.islice(it, batch_size))
-
+# 일부를 인덱스에 upsert한다.
 # for batch in chunks(zip(item_df.embedding_id, item_df.embedding), 50):
 #     index.upsert(vectors=batch)
 
-import time
+# 요렇게 해주면 된다.
+index.query(query_df[:1].embedding, top_k = 2)
+    # res = index.query(batch, top_k=10)  # issuing queries #이 부분 어떤 식으로 보내줘야하지?
+    # total_res += [res.matches for res in res.results] # 이 부분으로 결과를 받아온다.
 
-
-start = time.perf_counter()
-total_res = list()
-for batch in chunks(query_df.embedding,10):
-    res = index.query(batch, top_k=10)  # issuing queries
-    total_res += [res.matches for res in res.results]
-
-end = time.perf_counter()
-print("Run this test on a fast network to get the best performance.")
 
 # from torchvision import transforms as ts
 # import torchvision.models as models
